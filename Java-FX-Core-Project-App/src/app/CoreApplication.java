99786a93e0fb08e7e4b1e24c87cdb75549b306f7
Simon Blaha <s.blaha@live.at>
2019-06-24 11:41:52 +0200

    first push

---
+++ Java-FX-Core-Project-App/src/app/CoreApplication.java
@@ -0,0 +1,15 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CoreApplication extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
---
+++ Java-FX-Core-Project-App/src/db_conn/DataAccess.java
@@ -0,0 +1,82 @@
+package db_conn;
+
+import javafx.collections.ObservableList;
+
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
+
+public class DataAccess {
+
+    private Connection conn;
+    private static final String teacherTable = "teacher";
+
+    public DataAccess()
+            throws SQLException, ClassNotFoundException {
+
+        // Class.forName("org.hsqldb.jdbc.JDBCDriver" );
+
+        //STEP 2: Check if JDBC driver is available
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //STEP 3: Open a connection
+        System.out.println("Connecting to database...");
+        conn = DriverManager.getConnection(
+                "jdbc:mysql:173.212.235.205/benjamin_java_core_project" +
+                        "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                "benjamin_Benjo",
+                "");
+
+        // we will use this connection to write to a file
+        conn.setAutoCommit(true);
+        conn.setReadOnly(false);
+    }
+
+    public void closeDb() throws SQLException {
+        conn.close();
+    }
+
+    /**
+     * Get all db records
+     * @return
+     * @throws SQLException
+     */
+    public List<> getAllRows()  throws SQLException {
+
+        String sql = "SELECT * FROM " + teacherTable + " ORDER BY teacherName";
+        PreparedStatement pstmnt = conn.prepareStatement(sql);
+        ResultSet rs = pstmnt.executeQuery();
+        List<> list = new ArrayList<>();
+
+        while (rs.next()) {
+            int i = rs.getInt("teacherId");
+            String name = rs.getString("teacherName");
+            String surname = rs.getString("teacherSurname");
+            String email = rs.getString("teacherEmail");
+            list.add(new (i, name, surname, email));
+        }
+
+        pstmnt.close(); // also closes related result set
+        return list;
+    }
+
+    public List<> getAllRows2(int i) throws SQLException {
+
+        String sql = "SELECT class.classId, class.className FROM class INNER JOIN teacherclass ON class.classId = teacherclass.fk_classId WHERE teacherclass.fk_teacherId = ?";
+        PreparedStatement pstmnt = conn.prepareStatement(sql);
+        pstmnt.setInt(1, i);
+        ResultSet rs = pstmnt.executeQuery();
+        List<> listClasses = new ArrayList<>();
+
+        while (rs.next()) {
+            int i2 = rs.getInt("classId");
+            String name = rs.getString("className");
+            listClasses.add(new (i2, name));
+        }
+        pstmnt.close();
+        return listClasses;
+    }
+}
